e4920f4501794fe6363382659e14686f5ba10d0b
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-09-24 13:23:43 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let colorChangeId = null;
 
+startBtnRef.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  colorChangeId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtnRef.setAttribute('disabled', true);
+}
+
+stopBtnRef.addEventListener('click', onStopClick);
+
+function onStopClick() {
+  clearInterval(colorChangeId);
+  startBtnRef.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
