c3aeb937c754e6de41aa17b440d754da3038cd7a
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2022-09-28 16:43:12 +0000

    Deploying to gh-pages from @ Amfiteatr42/goit-js-hw-10@fb4899c71d6663b26119aeedd67ad7eb53c7eacf 🚀

--- 01-color-switcher.93528421.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled",!0)})),r.addEventListener("click",(function(){clearInterval(o),e.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.93528421.js.map
